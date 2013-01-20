b2950474c660c4c80d92f9f590cc5900284546ac
Allan Lavell <alavell@gmail.com>
2013-01-20 19:38:58 -0400

    latest

--- js/hud.js
+++ js/hud.js
@@ -1,8 +1,10 @@
 var foo = "bar";
 
-$(document).ready(function() {
+main = function() {
     $("a").click(function(event) {
         alert("sry bro, you can't go there");
         event.preventDefault();
     });
-});
+}
+
+$(document).ready(main);
