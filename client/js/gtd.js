5efb8bdc21e96ab336b49ef48d852ff444b3e448
cruncher19 <bradcovey@gmail.com>
2013-03-09 21:37:29 -0400

    GTD changes: registration working, not styled

--- client/js/gtd.js
+++ client/js/gtd.js
@@ -1,13 +1,22 @@
+Meteor.startup(function() {    
+    Session.set('mode', 'collect');
+    Session.set("loginOrReg", "Login: ");
+});
+
 Session.setDefault("username", "");
 Session.setDefault("password", "");
 
 Template.login.usernameAcquired = function() {
     return Session.get('username');
-}
+};
 
 Template.login.passwordAcquired = function() {
     return Session.get('password');
-}
+};
+
+Template.login.loginLabel = function() {
+    return Session.get("loginOrReg");
+};
 
 Template.login.events = {
     'keypress': function (event) {
@@ -17,22 +26,32 @@ Template.login.events = {
                 event.currentTarget.value = "";
             }
             else {
-                Meteor.loginWithPassword(Session.get('username'),
-                    event.currentTarget.value);
+                if(Session.equals("loginOrReg", "Login: ")) {                    
+                    Meteor.loginWithPassword(Session.get('username'), event.currentTarget.value);
+                } 
+                else if (Session.equals("loginOrReg", "Register: ")) {
+                    Accounts.createUser({username: Session.get("username"), password: event.currentTarget.value});
+                    session.set("loginOrReg", "Login: ");
+                }
                 event.currentTarget.value = "";
                 Session.set('username', '');
                 Meteor.flush();
             }
         }
+    },
+    'click .registerToggle': function (event) {
+        if (Session.get("loginOrReg") === "Login: ") {
+            Session.set("loginOrReg", "Register: ");
+        } else {
+            Session.set("loginOrReg", "Login: ");
+        }
     }
-}
+};
 
 Template.loggedIn.events = {
     'click': function(event) {
         Meteor.logout();
     }
-}
+};
 
-Meteor.startup(function() {
-    Session.set('mode', 'collect');
-});
+
